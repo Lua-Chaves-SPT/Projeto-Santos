06072f54bd458ceec5c6623327795c410c46b62b
LUA-CHAVES-SPT <lua.chaves@sptech.school>
2025-11-26 17:42:22 -0300

    Criação caminho da fkJOgador e inicio da pagina inciail

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -43,6 +43,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var jogadorFavorito = req.body.jogadorFavoritoServer;
 
 
     // Faça as validações dos valores
@@ -52,10 +53,14 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else {
+    } else if (jogadorFavorito == undefined){
+        res.status(400).send("Seu jogador favorito está undefined!")
+    }
+    
+    {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha,jogadorFavorito)
             .then(
                 function (resultado) {
                     res.json(resultado);
